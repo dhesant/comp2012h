0b32ef2c12c2cdb38c8f4c18fdb37f61c4820ecd
Dhesant Nakka <dhesant@gmail.com>
2015-09-04 16:55:35 +0800

    added card class, tested

--- PA1/blackjack.cpp
+++ PA1/blackjack.cpp
@@ -1,18 +1,34 @@
 #include <iostream> 
 #include <cstdlib> 
 #include <ctime> 
+#include <cstring>
 
-const char *number_lookup[] = { "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" };
-const char suit_lookup[] = "DCHS";
+const std::string number_lookup[] = { "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" };
+const std::string suit_lookup[] = {"D", "C", "H", "S"};
 
 int player_cash = 100;
 
-void get_random_card(int *number, int *suit) {
-  srand(time(0));  // initialize random number generator 
+class Card {
+public:
+  int number;
+  int suit;
+  Card() {
+    number = (double) rand()/RAND_MAX*13;
+    suit = (double) rand()/RAND_MAX*4;
+  }
+    
+  Card(int n, int s) {
+    number = n;
+    suit = s;
+  }
 
-  *number = rand()/RAND_MAX * 13;
-  *suit = rand()/RAND_MAX * 4;
-}
+  std::string getString() {
+    std::string s;
+    s = number_lookup[number];
+    s.append(suit_lookup[suit]);
+    return s;
+  }
+};
 
 void new_game(void) {
   int player_bet;
@@ -23,16 +39,13 @@ void new_game(void) {
   player_cash -= player_bet;
 
   //  std::cout << player_cash << endl; // Debug print
-
-  int cardNumber, cardSuit;
-  get_random_card(*cardNumber, *cardSuit);
-  
-  std::cout << cardNumber << std::endl << cardSuit << std::endl; // Debug print
+  Card test_card;
+  std::cout << "Debug: " << test_card.getString() << std::endl;
   
 }
 
 int main(void){ 
-
+  srand(time(0));  // initialize random number generator
   new_game();
   
   return 0;
