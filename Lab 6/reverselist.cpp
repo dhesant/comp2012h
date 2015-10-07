f76a1927ce70eeccfc25859ee5792674fcd42fe4
Dhesant Nakka <dhesant@gmail.com>
2015-10-07 18:30:56 +0800

    Created linkedList class

--- Lab 6/reverselist.cpp
+++ Lab 6/reverselist.cpp
@@ -1,11 +1,15 @@
 #include <cstdlib>
 #include <iostream>
 #include <fstream>
+#include <string>
 
 // Structures prototypes
 struct node;
 
 // Function prototypes
+
+// Class prototypes
+class linkedList;
 
 // Structures
 struct node {
@@ -15,9 +19,77 @@ struct node {
 
 // Functions
 
+// Classes
+class linkedList {
+private:
+  struct node *root;
+  struct node *iterator;
+public:
+  linkedList(char ch) {
+    root = (struct node *)malloc(sizeof(struct node));
+    iterator = root;
+    iterator->ch = ch;
+  }
+
+  void push_back(char ch) {
+    end();
+    iterator->next = (struct node *)malloc(sizeof(struct node));
+    iterator = iterator->next;
+    iterator->ch = ch;
+    iterator->next = 0;
+  }
+
+  char pop_back() { // Untested
+    struct node *prev;
+
+    iterator = root;
+    prev = root;
+    while (iterator->next != 0) {
+      prev = iterator;
+      iterator = iterator->next;
+    }
+    char ch = iterator->ch;
+    prev->next = 0;
+    
+    if (iterator != root) {
+      free(iterator);
+    }
+
+    iterator = root;
+
+    return ch;
+  }
+
+  void begin() {
+    iterator = root;
+  }
+
+  void end() {
+    iterator = root;
+    while(iterator->next != 0) {
+      iterator = iterator->next;
+    }
+  }
+  
+  char getCurrent() {
+    return iterator->ch;
+  }
+
+  void increment() {
+    iterator = iterator->next;
+  }
+  
+  bool isEnd() {
+    return (iterator->next == 0);
+  }
+};
+
 // Variables
 const char in_name[] = "input.txt";
 const char out_name[] = "output.txt";
+
+const char eol[] = "null;";
+const char delim[] = "->";
 
 int main() {
   std::ifstream infile;
@@ -26,25 +98,21 @@ int main() {
     std::cout << "An error has occured while opening 'input.txt'" << std::endl;
     return 1;
   }
+
+  linkedList list(infile.get());
   
-  struct node *root;
-  struct node *iterator;
-
-  root = (struct node *)malloc(sizeof(struct node));
-
-  iterator = root;
 
   while(!infile.eof()) {
-    infile>>iterator->ch;
-    iterator->next = (struct node *)malloc(sizeof(struct node));
-    iterator = iterator->next;
+    list.push_back(infile.get());
   }
-  iterator->next = 0;
 
-  iterator = root;
-  while (iterator->next != 0) {
-    std::cout << iterator->ch;
-    iterator = iterator->next;
+  list.begin();
+  while(true) {
+    std::cout << list.getCurrent();
+    list.increment();
+    if(list.isEnd()) {
+      break;
+    }
   }
 
   return 0;
