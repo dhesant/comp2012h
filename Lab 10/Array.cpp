24f0716a89c6f94c4647b2f5d3d8b43a1f48be3e
Dhesant Nakka <dhesant@gmail.com>
2015-11-04 13:43:29 +0800

    Lab 10: Finish part 1, int array

--- Lab 10/Array.cpp
+++ Lab 10/Array.cpp
@@ -3,86 +3,101 @@
 /* Constructors & Destructor */
 Array::Array()
 {
-    _data = NULL;
-    _size = 0;
+  _data = NULL;
+  _size = 0;
 }
 
 Array::Array (int size)
 {
-    assert (size >= 0);
-    _size = size;
-    _data = new T [_size];
+  assert (size >= 0);
+  _size = size;
+  _data = new T [_size];
 }
 
 
 Array::Array (const Array& arr)
 {
-    assert (arr.IsValid ());
-    _size = arr._size;
-    _data = new T [_size];
-    for (int i = 0; i < _size; i++)
+  assert (arr.IsValid ());
+  _size = arr._size;
+  _data = new T [_size];
+  for (int i = 0; i < _size; i++)
     {
-        _data[i] = arr._data[i];
+      _data[i] = arr._data[i];
     }
 }
 
 Array::Array (const T * arr, int size)
 {
-    assert(arr != NULL && size > 0);
+  assert(arr != NULL && size > 0);
 
-    _size = size;
-    _data = new T [_size];
-    for (int i = 0; i < _size; i++)
+  _size = size;
+  _data = new T [_size];
+  for (int i = 0; i < _size; i++)
     {
-        _data[i] = arr[i];
+      _data[i] = arr[i];
     }
 }
 
 
 Array::~Array ()
 {
-    if (_data != NULL && _size > 0)
+  if (_data != NULL && _size > 0)
     {
-        delete [] _data;
-        _data = NULL;
-        _size = 0;
+      delete [] _data;
+      _data = NULL;
+      _size = 0;
     }
 }
 
 /* Operators Overloaded */
 // The main task
-Array& Array::operator=(const Array& arr)
-{
-    assert(arr.IsValid ());
-    // TO DO: complete the operator "="
-    // ...
+Array& Array::operator=(const Array& arr) {
+  assert(arr.IsValid ());
+
+  delete [] _data;
+
+  _size = arr.Size();
+  _data = new T [_size];
+
+  for (int i = 0; i < _size; i++) {
+    _data[i] = arr[i];
+  }
+  
+  return *this;
 }
 
-bool Array::operator==(const Array& arr) const
-{
-    // TO DO: complete the operator "=="
-    // ...
+bool Array::operator==(const Array& arr) const {
+  //  assert(arr.IsValid ());
+  if (arr.Size() != _size) {
+    return false;
+  }
+  for (int i = 0; i < _size; i++) {
+    if (arr[i] != _data[i]) {
+      return false;
+    }
+  }
+  return true;
 }
 
-T& Array::operator[](int index)
-{
-    assert (IsValid ());
-    assert (index >=0 && index < _size);
-    // TO DO: complete the operator "[]"
-    // ...
+T& Array::operator[](int index) {
+  assert (IsValid ());
+  assert (index >=0 && index < _size);
+  return _data[index];
 }
 
-T Array::operator[](int index) const
-{
-    assert (IsValid ());
-    assert (index >=0 && index < _size);
-    // TO DO: complete the operator "[]"
-    // ...
+T Array::operator[](int index) const {
+  assert (IsValid ());
+  assert (index >=0 && index < _size);
+  return _data[index];
 }
 
 // TO DO: implement the operator "+="
-// ...
-
+// ... 
+/*
+  Array& operator+=(const Array& arr) {
+  assert(arr.IsValid ());
+  }
+*/
 // ...
 // End operator +=
 
@@ -90,34 +105,34 @@ T Array::operator[](int index) const
 /* Methods */
 void Array::Append (const T& item)
 {
-   if (_data != NULL)
+  if (_data != NULL)
     {
-        T* temp = new T [_size+1];
-        for (int i = 0; i<_size; i++)
+      T* temp = new T [_size+1];
+      for (int i = 0; i<_size; i++)
         {
-            temp[i] = _data[i];
+	  temp[i] = _data[i];
         }
-        temp[_size] = item;
-        delete [] _data;
-        _data = temp;
-        _size++;
+      temp[_size] = item;
+      delete [] _data;
+      _data = temp;
+      _size++;
     }
-    else
+  else
     {
-        _size = 1;
-        _data = new T [_size];
-        _data[0] = item;
+      _size = 1;
+      _data = new T [_size];
+      _data[0] = item;
     }
 
 }
 
 /* Properties */
- bool Array::IsValid () const
+bool Array::IsValid () const
 {
-    return (_data != NULL && _size > 0);
+  return (_data != NULL && _size > 0);
 }
 
 int Array::Size () const
 {
-    return _size;
+  return _size;
 }
