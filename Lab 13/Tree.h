e10efa65663630740eca1272fd4fbe8e7457b7e6
Dhesant Nakka <dhesant@gmail.com>
2015-11-12 20:29:22 +0800

    Lab 13: finished all tasks

--- Lab 13/Tree.h
+++ Lab 13/Tree.h
@@ -11,195 +11,186 @@ using std::cout;
 template< typename NODETYPE >
 class Tree 
 {
-public:
-   Tree();
-   void insertNode( const NODETYPE & );
-   void preOrderTraversal() const;
-   void inOrderTraversal() const;
-   void postOrderTraversal() const;
-   int getDepth() const;
-   TreeNode< NODETYPE > *binaryTreeSearch( int ) const;
-protected:
-   TreeNode< NODETYPE > *rootPtr;
-private:
-   // utility functions
-   void insertNodeHelper( TreeNode< NODETYPE > **, const NODETYPE & );
-   void preOrderHelper( TreeNode< NODETYPE > * ) const;
-   void inOrderHelper( TreeNode< NODETYPE > * ) const;
-   void postOrderHelper( TreeNode< NODETYPE > * ) const;
-   void determineDepth( TreeNode< NODETYPE > *, int *, int * ) const;
-   TreeNode< NODETYPE > 
-      *binarySearchHelper( TreeNode< NODETYPE > *, int ) const;
+ public:
+  Tree();
+  void insertNode( const NODETYPE & );
+  void preOrderTraversal() const;
+  void inOrderTraversal() const;
+  void postOrderTraversal() const;
+  int getDepth() const;
+  TreeNode< NODETYPE > *binaryTreeSearch( int ) const;
+ protected:
+  TreeNode< NODETYPE > *rootPtr;
+ private:
+  // utility functions
+  void insertNodeHelper( TreeNode< NODETYPE > **, const NODETYPE & );
+  void preOrderHelper( TreeNode< NODETYPE > * ) const;
+  void inOrderHelper( TreeNode< NODETYPE > * ) const;
+  void postOrderHelper( TreeNode< NODETYPE > * ) const;
+  void determineDepth( TreeNode< NODETYPE > *, int *, int * ) const;
+  TreeNode< NODETYPE > 
+    *binarySearchHelper( TreeNode< NODETYPE > *, int ) const;
 }; // end class Tree
 
 // constructor
 template< typename NODETYPE >
 Tree< NODETYPE >::Tree() 
 { 
-   rootPtr = 0; 
+  rootPtr = 0; 
 } // end Tree constructor
 
 // begin inserting node into Tree
 template< typename NODETYPE >
 void Tree< NODETYPE >::insertNode( const NODETYPE &value )
 { 
-   insertNodeHelper( &rootPtr, value ); 
+  insertNodeHelper( &rootPtr, value ); 
 } // end function insertNode
 
 // This function receives a pointer to a pointer so the
 // pointer can be modified.
 template< typename NODETYPE >
 void Tree< NODETYPE >::insertNodeHelper( TreeNode< NODETYPE > **ptr,
-   const NODETYPE &value )
+					 const NODETYPE &value )
 {
-   if ( *ptr == 0 ) // tree is empty
-      *ptr = new TreeNode< NODETYPE >( value );
-   else // tree is not empty
-   {  
+  if ( *ptr == 0 ) // tree is empty
+    *ptr = new TreeNode< NODETYPE >( value );
+  else // tree is not empty
+    {  
       // data to insert is less than data in current node
       if ( value < ( *ptr )->data )
-         insertNodeHelper( &( ( *ptr )->leftPtr ), value );
+	insertNodeHelper( &( ( *ptr )->leftPtr ), value );
  
-     // ===== TO-DO ======
+      else if ( value > (*ptr)->data )
+	insertNodeHelper( &( ( *ptr )->rightPtr ), value );
 
-     // if data to insert is larger than data in current node
-	 // insert it to the right subtree
-	 // note: duplicated values should NOT be inserted
-
-   } // end else
+    } // end else
 } // end function insertNodeHelper
 
 // begin preorder traversal of Tree
 template< typename NODETYPE >
 void Tree< NODETYPE >::preOrderTraversal() const 
 { 
-   preOrderHelper( rootPtr ); 
+  preOrderHelper( rootPtr ); 
 } // end function preOrderTraversal
 
 // utility function to perform preorder traversal of Tree
 template< typename NODETYPE >
 void Tree< NODETYPE >::preOrderHelper( TreeNode< NODETYPE > *ptr ) const
 {
-   if ( ptr != 0 ) 
-   {
-	  // ===== TO-DO ======
-
+  if ( ptr != 0 ) 
+    {
       cout << ptr->data << ' '; // process node
-      // go to left subtree
-      // go to right subtree
+      preOrderHelper(ptr->leftPtr); // go to left subtree
+      preOrderHelper(ptr->rightPtr); // go to right subtree
 
-   } // end if
+    } // end if
 } // end function preOrderHelper
 
 // begin inorder traversal of Tree
 template< typename NODETYPE >
 void Tree< NODETYPE >::inOrderTraversal() const 
 { 
-   inOrderHelper( rootPtr ); 
+  inOrderHelper( rootPtr ); 
 } // end function inOrderTraversal
 
 // utility function to perform inorder traversal of Tree
 template< typename NODETYPE >
 void Tree< NODETYPE >::inOrderHelper( TreeNode< NODETYPE > *ptr ) const
 {
-   if ( ptr != 0 ) 
-   {
-	  // ===== TO-DO ======
+  if ( ptr != 0 ) 
+    {
 
-      // go to left subtree
+      inOrderHelper(ptr->leftPtr); // go to left subtree
       cout << ptr->data << ' '; // process node
-      // go to right subtree
+      inOrderHelper(ptr->rightPtr); // go to right subtree
 
-   } // end if
+    } // end if
 } // end function inOrderHelper
 
 // begin postorder traversal of Tree
 template< typename NODETYPE >
 void Tree< NODETYPE >::postOrderTraversal() const 
 { 
-   postOrderHelper( rootPtr ); 
+  postOrderHelper( rootPtr ); 
 } // end function postOrderTraversal
 
 // utility function to perform postorder traversal
 template< typename NODETYPE >
 void Tree< NODETYPE >::postOrderHelper( TreeNode< NODETYPE > *ptr ) const
 {
-   if ( ptr != 0 ) 
-   {
-	   // ===== TO-DO ======
+  if ( ptr != 0 ) 
+    {
+      postOrderHelper(ptr->leftPtr); // go to left subtree
+      postOrderHelper(ptr->rightPtr);// go to right subtree
+      cout << ptr->data << ' '; // process node
 
-       // go to left subtree
-       // go to right subtree
-       cout << ptr->data << ' '; // process node
-
-   } // end if
+    } // end if
 } // end postOrderHelper
 
 // get the depth of the tree
 template< typename NODETYPE >
 int Tree< NODETYPE >::getDepth() const
 {
-   int totalDepth = 0;
-   int currentDepth = 0;
+  int totalDepth = 0;
+  int currentDepth = 0;
 
-   determineDepth( rootPtr, &totalDepth, &currentDepth );
-   return totalDepth;
+  determineDepth( rootPtr, &totalDepth, &currentDepth );
+  return totalDepth;
 } // end function getDepth
 
 // calculate the depth of the tree
 template< typename NODETYPE >
 void Tree< NODETYPE >::determineDepth( TreeNode< NODETYPE > *ptr,
-   int *totPtr, int *currPtr ) const
+				       int *totPtr, int *currPtr ) const
 {   
-   if ( ptr != 0 ) // until ptr points to 0
-   {
+  if ( ptr != 0 ) // until ptr points to 0
+    {
       ( *currPtr )++;
 
-	  // ===== TO-DO ======
-	  
-	  // check if current depth is larger than total depth
-	  // if so, update total depth to current depth
+      if ( (*currPtr) > (*totPtr))
+	(*totPtr) = (*currPtr);
 
-	  // determine depth on the left subtreee
-	  // determin depth on the right subtree
+      determineDepth(ptr->leftPtr, totPtr, currPtr);
+      determineDepth(ptr->rightPtr, totPtr, currPtr);
 
       ( *currPtr )--;
-   } // end if
+    } // end if
 } // end function determineDepth
 
 // begin binary search
 template< typename NODETYPE >
 TreeNode< NODETYPE > *Tree< NODETYPE >::binaryTreeSearch( int val ) const
 { 
-   return binarySearchHelper( rootPtr, val ); 
+  return binarySearchHelper( rootPtr, val ); 
 } // end function binaryTreeSearch
 
 // do a binary search on the Tree
 template< typename NODETYPE >
 TreeNode< NODETYPE > *Tree< NODETYPE >::binarySearchHelper( 
-   TreeNode< NODETYPE > *ptr, int value ) const
+							   TreeNode< NODETYPE > *ptr, int value ) const
 {
-   // if value is not found
-   if ( ptr == 0 )
-      return 0;
+  // if value is not found
+  if ( ptr == 0 )
+    return 0;
 
-   cout << "Comparing " << value << " to " << ptr->getData();
+  cout << "Comparing " << value << " to " << ptr->getData();
 
-   // ===== TO-DO ======
+  if ( value == ptr->getData() ) {
+    cout << "; search complete\n";
+    return ptr;
+  }
 
-   // if search value matches current data
-   // output "; search complete\n"
-   // and return the node storing the matched item
+  else if ( value < ptr->getData() ) {
+    cout << "; smaller, walk left\n";
+    return binarySearchHelper (ptr->leftPtr, value);
+  }
 
-   // if search value is less than current data
-   // output "; smaller, walk left\n";
-   // and recursively perform binary sarch on the left subtree
+  else if ( value > ptr->getData() ) {
+    cout << "; larger, walk right\n";
+    return binarySearchHelper (ptr->rightPtr, value);
+  }
 
-   // if search value is greater than current data
-   // output "; larger, walk right\n";
-   // and recursively perform binary sarch on the right subtree
-
-   return 0;
+  return 0;
 
 } // end function binarySearchHelper
 
